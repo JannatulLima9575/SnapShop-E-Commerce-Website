4b10dd2dd4c2a60255aa39450059e9f80b4bf51b
Jannatul <limajannat1675@gmail.com>
2025-03-07 11:22:33 +0600

    added user section

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let search = document.querySelector('.search-box');
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
     cart.classList.remove('active');
+    user.classList.remove('active');
 }
 
 // for cart section
@@ -12,4 +13,14 @@ let cart = document.querySelector('.cart');
 document.querySelector('#cart-icon').onclick = () => {
     cart.classList.toggle('active');
     search.classList.remove('active');
+    user.classList.remove('active');
+}
+
+// for user section
+let user = document.querySelector('.user');
+
+document.querySelector('#user-icon').onclick = () => {
+    user.classList.toggle('active');
+    search.classList.remove('active');
+    cart.classList.remove('active');
 }
