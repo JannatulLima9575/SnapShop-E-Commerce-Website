870b9d06f76ad7cfdea993b68fcc37244695e23c
Jannatul <limajannat1675@gmail.com>
2025-03-07 10:44:47 +0600

    added cart section

--- main.js
+++ main.js
@@ -3,4 +3,13 @@ let search = document.querySelector('.search-box');
 
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
+    cart.classList.remove('active');
+}
+
+// for cart section
+let cart = document.querySelector('.cart');
+
+document.querySelector('#cart-icon').onclick = () => {
+    cart.classList.toggle('active');
+    search.classList.remove('active');
 }
